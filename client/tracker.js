491a87e976eb87286e3dfb423ec967006fa6af5a
William Leong <willjleong@gmail.com>
2015-04-20 14:20:17 -0700

    replace tracker.coffee with tracker.js

---
+++ client/tracker.js
@@ -0,0 +1,15 @@
+/**
+ * Created by williamleong on 4/20/15.
+ */
+
+Tracker.autorun(function() {
+  if (Meteor.userId() && !_.isNull(Router.current()) && Router.current().route.name === 'entrySignIn') {
+    Router.go('dashboard');
+  }
+  if (Meteor.userId() && !_.isNull(Router.current()) && Router.current().route.name === 'entrySignUp') {
+    Router.go('dashboard');
+  }
+  Meteor.subscribe('user');
+  Meteor.subscribe('users');
+  return Meteor.subscribe('userPicture');
+});
